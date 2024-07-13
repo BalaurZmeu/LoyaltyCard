65620f2b7a1ff8fb49d7517264763fa53b76b3c2
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-13 14:42:52 +0300

    Added function generate_id to utils, changed models

--- loyalty_card/card_manager/models.py
+++ loyalty_card/card_manager/models.py
@@ -1,24 +1,21 @@
 from django.db import models
 from datetime import datetime
-import uuid
-from .utils import generate_series
-
+from .utils import generate_series, generate_id
 
 class Card(models.Model):
     """Model representing a specific loyalty card."""
     
     series = models.CharField(max_length=2, default=generate_series, help_text="Series of the card")
     
-    id_number = models.UUIDField(
+    id_number = models.BigIntegerField(
         primary_key=True,
-        default=uuid.uuid4,
+        default=generate_id,
         help_text="Unique ID for this particular card across the whole system"
     )
     
     issued = models.DateTimeField(auto_now_add=True)
     expires = models.DateTimeField(null=True, blank=True)
     last_used = models.DateTimeField(null=True, blank=True)
-    
     amount = models.FloatField(help_text="The amount of bonus points granted to a customer")
     
     CARD_STATUS = (
@@ -35,7 +32,7 @@ class Card(models.Model):
         help_text="Status of the card",
     )
     
-    def is_expired(self):
+    def check_if_expired(self):
         """Determines if the card is expired based on expiration date and current date.
         If card is expired, changes the status of the card to 'expired' and returns True."""
         if self.expires and datetime.now() > self.expires:
--- loyalty_card/card_manager/utils.py
+++ loyalty_card/card_manager/utils.py
@@ -1,12 +1,27 @@
-from random import choices
+from random import choices, randint
 from string import ascii_uppercase as letters
-from .models import Card
 
 
 def generate_series():
-    """This function generates a unique two uppercase letters series."""
+    """This function generates a two uppercase letters series.
+    It will keep generating a new series until it finds one that does
+    not already exist in the Card model's series field."""
+    # Moved import inside the function to avoid circular import issue
+    from .models import Card
     while True:
         new_series = ''.join(choices(letters, k=2))
         if not Card.objects.filter(series=new_series).exists():
             return new_series
+
+
+def generate_id():
+    """This function generates a 10 digits long random integer.
+    It will keep generating a new id until it finds one that does
+    not already exist in the Card model's id_number field."""
+    # Moved import inside the function to avoid circular import issue
+    from .models import Card
+    while True:
+        new_id = randint(10**9, 10**10 - 1)
+        if not Card.objects.filter(id_number=new_id).exists():
+            return new_id
 
