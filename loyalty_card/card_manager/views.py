5fa8f2882617cbf27e2803dc8ad0432fb75769ea
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-15 17:01:11 +0300

    Working on views

--- loyalty_card/card_manager/views.py
+++ loyalty_card/card_manager/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Card
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    return render(request, 'index.html', context=context)
+
+
+class CardListView(LoginRequiredMixin, generic.ListView):
+    model = Card
+    paginate_by = 30
+
+
+class CardDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Card
+
