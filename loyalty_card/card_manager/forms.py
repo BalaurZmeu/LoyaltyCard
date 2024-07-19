26d5f9ed13c9722663c659ab4cf3c4dd4e34c229
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-19 17:05:50 +0300

    Made the table look pretty on mobile

--- loyalty_card/card_manager/forms.py
+++ loyalty_card/card_manager/forms.py
@@ -4,7 +4,7 @@ from .models import Card
 class CardSearchForm(forms.Form):
     series = forms.CharField(required=False, label='Series')
     id_number = forms.CharField(required=False, label='Number')
-    issued = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Issued Date')
-    expires = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Expiration Date')
+    issued = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Issued')
+    expires = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Expires')
     status = forms.ChoiceField(choices=[('', '---')]+list(Card.CARD_STATUS), required=False, label='Status')
 
