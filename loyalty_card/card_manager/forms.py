b899b81fca22ab851d0e19b3f37ff04fa1885c26
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-20 03:36:14 +0300

    Changed forms.py to make search form look pretty

--- loyalty_card/card_manager/forms.py
+++ loyalty_card/card_manager/forms.py
@@ -2,9 +2,59 @@ from django import forms
 from .models import Card
 
 class CardSearchForm(forms.Form):
-    series = forms.CharField(required=False, label='Series')
-    id_number = forms.CharField(required=False, label='Number')
-    issued = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Issued')
-    expires = forms.DateField(required=False, widget=forms.SelectDateWidget, label='Expires')
-    status = forms.ChoiceField(choices=[('', '---')]+list(Card.CARD_STATUS), required=False, label='Status')
+    series = forms.CharField(
+        required=False, 
+        label='Series',
+        widget=forms.TextInput(
+            attrs={
+                'style': 'width: 300px;',
+                'class': 'form-control',
+            }
+        )
+    )
+    
+    id_number = forms.CharField(
+        required=False,
+        label='Number',
+        widget=forms.TextInput(
+            attrs={
+                'style': 'width: 300px;',
+                'class': 'form-control',
+            }
+        )
+    )
+    
+    status = forms.ChoiceField(
+        choices=[('', '---')]+list(Card.CARD_STATUS),
+        required=False,
+        label='Status',
+        widget=forms.Select(
+            attrs={
+                'style': 'width: 300px;',
+                'class': 'form-control',
+            }
+        )
+    )
+    
+    issued = forms.DateField(
+        required=False,
+        label='Issued',
+        widget=forms.SelectDateWidget(
+            attrs={
+                'style': 'width: 300px;',
+                'class': 'form-control',
+            }
+        )
+    )
+    
+    expires = forms.DateField(
+        required=False,
+        label='Expires',
+        widget=forms.SelectDateWidget(
+            attrs={
+                'style': 'width: 300px;',
+                'class': 'form-control',
+            }
+        )
+    )
 
